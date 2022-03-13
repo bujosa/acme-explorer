bee1365fb39085687acbe2891277ebb6ca85e72c
David Bujosa <davidbujosa@gmail.com>
2022-03-13 17:30:16 +0100

    Add Grafana and K6 setup #22 (#25)
    * feat: add grafana dashboard
    
    * feat: add k6 script example

---
+++ extra/k6/main.js
@@ -0,0 +1,33 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+import { stages } from "./stages.js";
+
+const selectedStages = stages[__ENV.STAGE_ID] || {
+  stages: undefined,
+  name: "|default|",
+};
+
+export const options = {
+  stages: selectedStages.config,
+  thresholds: { http_req_duration: ["avg<500", "p(95)<1500"] },
+  userAgent: "K6TestingUserAgent/1.0",
+};
+
+export default function () {
+  const url = `${__ENV.HOSTNAME}/v1/finders`;
+
+  const params = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    tags: {
+      name: `${selectedStages.name} with trips`,
+    },
+  };
+
+  const res = http.get(url, params);
+
+  check(res, { "is status 200": (r) => r.status === 200 });
+
+  sleep(1);
+}
